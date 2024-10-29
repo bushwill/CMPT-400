3d91c33feb3024d0a0456e598ac1bbd8b04b2a43
Will Bushell <williambushell7@gmail.com>
2024-10-29 15:22:00 -0600

    not publishing personal info this time

--- DFABuilder_OpenAI_Interface.py
+++ DFABuilder_OpenAI_Interface.py
@@ -1,14 +1,15 @@
 from openai import OpenAI
-client = OpenAI(
-    api_key='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',
-)
-
 def makeRequest(request):
+    client = OpenAI(api_key=getKey())
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{"role": "system", "content": "You are a strictly mathematical model that outputs DFA definitions in a consistent single line with no formatting. For example, a DFA that accepts 'a' is defined as DFA = {{q0, q1}, {a}, {(0, a, 1)}, q0, {1}}."},{"role": "user", "content": request}],
     )
     return response.choices[0].message.content
+
+def getKey():
+    f = open("APIKey.txt", "r")
+    return f.readline().strip()
 
 def main():
     request = "AAA or BBB"
